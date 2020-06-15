74db16a558915873dcf7b83d1c116388f810cc6b
natalia <natalia1528alonso@gmail.com>
2020-06-15 14:07:57 +0200

    modelos SAR/SMA/SARMA

--- machine_learning_series_temporales.R
+++ machine_learning_series_temporales.R
@@ -15,7 +15,8 @@ library(urca)
 
 # install.packages("lmtest")
 # install.packages("urca")
-setwd("C:/Users/susi_/Desktop/Machine Learning2/Series_Temporales/practica_local")
+# setwd("C:/Users/susi_/Desktop/Machine Learning2/Series_Temporales/practica_local")
+
 Sys.setlocale ("LC_TIME", 'English')
 data <- read.csv("amazon.csv")
 
@@ -51,8 +52,8 @@ fires_rio %>%
   xlab("Year") + ylab("Fires")
 
 
-fires_train <- fires_rio %>% filter_index(. ~ "2015-12")
-fires_test <- fires_rio %>% filter_index(2016 ~ .)
+fires_train <- fires_rio %>% filter_index("1999 Q1" ~ "2013 Q4")
+fires_test <- fires_rio %>% filter_index("2014 Q1" ~ "2015 Q4")
 
 fires_train %>%
   gg_season(value, labels = "right")
@@ -79,9 +80,12 @@ lambda # lambda próximo a cero -> transformacion log
 fires_train %>% autoplot(log(value)) +
   labs(y = "Log transformed")
 
+# Lambda -0.33: transformacion logaritmica
+
 #### Estacionariedad en media: tb hay que analizar si aplicar una diferencia para estabilizar la media ####
 
 # Transformación logarítmica no válida: da NA, probamos tb sin log()
+
 
 fires_train %>% features(log(value), unitroot_kpss)
 fires_train %>% features(difference(log(value), 1), unitroot_kpss)
@@ -92,62 +96,64 @@ fires_train %>%
   mutate(log_turnover = difference(log(value),1)) %>%
   features(log_turnover, unitroot_nsdiffs)
 
-# Probamos sin log:
-fires_train %>% features((value), unitroot_kpss)
-fires_train %>% features(difference((value), 1), unitroot_kpss)
-fires_train %>% autoplot(difference(value, 1)) 
-fires_train %>%
-  mutate(log_turnover = difference((value),1)) %>%
-  features(log_turnover, unitroot_nsdiffs) # como es menor que 0.64 no indica diferencia estacional
-
 #### Autocorrelación ####
 
+par(mfrow=c(2,1)) 
+
 # estimate of the autocorrelation function of a (possibly multivariate) time series: ACF
-acf(fires_train,lag.max=140)
-fires_train %>% ACF(value, lag_max = 140) %>% autoplot()
+fires_train %>% 
+  ACF(log(value), lag_max = 100) %>% 
+  autoplot()
 
 # estimate of the partial autocorrelation function of a (possibly multivariate) time series: PACF
-pacf(fires_train, lag.max=140)
-fires_train %>% PACF(value, lag_max = 140) %>% autoplot()
+fires_train %>% 
+  PACF(log(value), lag_max = 100) %>% 
+  autoplot()
+
+# fires_train %>% gg_tsdisplay(value %>% difference(1) %>% difference(), plot_type='partial')
 
 #### Determinación del modelo ####
 
-fitARIMA <- arima(fires_train$value, 
-                  order=c(1,1,1), 
-                  seasonal = list(order = c(1,0,0), period = 4), 
-                  method="ML")
 
+# SAR: p=2/P=1 con PACF (q=0/Q=0) (d=1/D=1) (2,1,0)(1,1,0)4
+fit <- fires_train %>% model(arima = ARIMA(log(value) ~ pdq(2,0,0) + PDQ(1,0,0, period=4)))
+report(fit)
 
+# SMA: q=2/Q=1 con ACF (p=0/P=0) (d=1/D=1) (0,1,2)(0,1,1)4
+fit <- fires_train %>% model(arima = ARIMA(log(value) ~ pdq(0,0,2) + PDQ(0,0,1, period=4)))
+report(fit)
 
-coeftest(fitARIMA) 
+# SARIMA p=2/P=1 con PACF; d=1/D=1; q=2/Q=1 con ACF;  (2,1,2)(1,1,1)4
+fit <- fires_train %>% model(arima = ARIMA(log(value) ~ pdq(2,1,2) + PDQ(1,1,1, period=4)))
+report(fit)
 
 ### Fase de estimación y contraste ###
+# 
+# my_tsresiduals <- function (data, ...) {
+#   if (!fabletools::is_mable(data)) {
+#     abort("gg_tsresiduals() must be used with a mable containing only one model.")
+#   }
+#   data <- stats::residuals(data)
+#   if (n_keys(data) > 1) {
+#     abort("gg_tsresiduals() must be used with a mable containing only one model.")
+#   }
+#   gg_tsdisplay(data, !!sym(".resid"), plot_type = "partial",
+#                ...)
+# }
 
-my_tsresiduals <- function (data, ...) {
-  if (!fabletools::is_mable(data)) {
-    abort("gg_tsresiduals() must be used with a mable containing only one model.")
-  }
-  data <- stats::residuals(data)
-  if (n_keys(data) > 1) {
-    abort("gg_tsresiduals() must be used with a mable containing only one model.")
-  }
-  gg_tsdisplay(data, !!sym(".resid"), plot_type = "partial",  
-               ...)
-}
-
-fit2 <- fires_train %>%
-  model(arima = ARIMA(value ~ pdq(0,1,0) + PDQ(1,1,0, period= 7)))
-fit2 %>% my_tsresiduals(lag_max =36)
-report(fit2)
-
-fit3 <- fires_train %>%
-  model(arima = ARIMA(value ~ pdq(0,1,0) + PDQ(1,1,1)))
-fit3 %>% my_tsresiduals(lag_max =36)
-report(fit3)
-
-
-fit4 <- fires_train %>%
-  model(arima = ARIMA(value ~ pdq(0,1,2) + PDQ(1,1,1)))
-fit4 %>% my_tsresiduals(lag_max =36)
-report(fit4)
+# fit2 <- fires_train %>%
+#   model(arima = ARIMA(value ~ pdq(0,1,0) + PDQ(1,1,0, period= 7)))
+# fit2 %>% my_tsresiduals(lag_max =36)
+# report(fit2)
+# 
+# fit3 <- fires_train %>%
+#   model(arima = ARIMA(value ~ pdq(0,1,0) + PDQ(1,1,1)))
+# fit3 %>% my_tsresiduals(lag_max =36)
+# report(fit3)
+# 
+# 
+# fit4 <- fires_train %>%
+#   model(arima = ARIMA(value ~ pdq(0,1,2) + PDQ(1,1,1)))
+# fit4 %>% my_tsresiduals(lag_max =36)
+# report(fit4)
 
